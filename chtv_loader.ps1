5acfc48fa879cf0b61d9c18d12bd68d2b4198778
chahertv <chaherbbc@gmail.com>
2026-05-27 22:31:13 +0100

    Update chtv_loader.ps1

--- chtv_loader.ps1
+++ chtv_loader.ps1
@@ -1,39 +1,43 @@
-$cfg_url = "https://raw.githubusercontent.com/chahertv/stream-channels/main/config.json"
-$chs_url = "https://raw.githubusercontent.com/chahertv/stream-channels/main/channels.json"
-$out = "$env:TEMP\chtv_vars.bat"
+param([string]$TMP_J, [string]$TMP_V)
+try {
+    $j = Get-Content $TMP_J -Raw -Encoding UTF8 | ConvertFrom-Json
+    $b = $j.bat
+    $lines = New-Object System.Collections.Generic.List[string]
 
-$c = Invoke-WebRequest $cfg_url -UseBasicParsing | ConvertFrom-Json
-$h = Invoke-WebRequest $chs_url -UseBasicParsing | ConvertFrom-Json
-$b = $c.bat
+    $lines.Add("SET TC=" + $j.channels.Count)
+    $lines.Add("SET B_COLOR_MAIN=" + $b.color_main)
+    $lines.Add("SET B_COLOR_PLAYER=" + $b.color_player)
+    $lines.Add("SET B_TITLE_MAIN=" + $b.title_main)
+    $lines.Add("SET B_TITLE_PLAYER=" + $b.title_player)
+    $lines.Add("SET B_LINE1=" + $b.header_line1)
+    $lines.Add("SET B_LINE2=" + $b.header_line2)
+    $lines.Add("SET B_LINE3=" + $b.header_line3)
+    $lines.Add("SET B_MENU_HEADER=" + $b.menu_header)
+    $lines.Add("SET B_PROMPT=" + $b.prompt_msg)
+    $lines.Add("SET B_INVALID=" + $b.invalid_msg)
+    $lines.Add("SET B_LAUNCHING=" + $b.launching_msg)
+    $lines.Add("SET B_PLAYER_HDR=" + $b.player_header)
+    $lines.Add("SET B_PLAYER_STS=" + $b.player_status)
+    $lines.Add("SET B_PLAYER_FTR=" + $b.player_footer)
+    $lines.Add("SET B_BYE1=" + $b.bye_line1)
+    $lines.Add("SET B_BYE2=" + $b.bye_line2)
+    $lines.Add("SET B_BYE3=" + $b.bye_line3)
+    $lines.Add("SET B_FOOTER=" + $b.footer_line)
+    $lines.Add("SET B_OFFLINE=" + $b.offline_label)
+    $lines.Add("SET B_CHAN_OFF=" + $b.channel_offline_msg)
+    $lines.Add("SET B_ERROR=" + $b.error_msg)
 
-$lines = @()
-$lines += "SET C="   + $b.color_main
-$lines += "SET CP="  + $b.color_player
-$lines += "SET T="   + $b.title_main
-$lines += "SET TP="  + $b.title_player
-$lines += "SET L1="  + $b.header_line1
-$lines += "SET L2="  + $b.header_line2
-$lines += "SET L3="  + $b.header_line3
-$lines += "SET MH="  + $b.menu_header
-$lines += "SET PM="  + $b.prompt_msg
-$lines += "SET IV="  + $b.invalid_msg
-$lines += "SET LM="  + $b.launching_msg
-$lines += "SET PH="  + $b.player_header
-$lines += "SET PS2=" + $b.player_status
-$lines += "SET PF="  + $b.player_footer
-$lines += "SET B1="  + $b.bye_line1
-$lines += "SET B2="  + $b.bye_line2
-$lines += "SET B3="  + $b.bye_line3
-$lines += "SET FT="  + $b.footer_line
-$lines += "SET TC="  + $h.channels.Count
+    $i = 1
+    foreach ($c in $j.channels) {
+        $lines.Add("SET N" + $i + "=" + $c.name)
+        $lines.Add("SET U" + $i + "=" + $c.url)
+        $lines.Add("SET X" + $i + "=" + $(if ($c.active) { "1" } else { "0" }))
+        $i++
+    }
 
-$i = 1
-foreach ($ch in $h.channels) {
-    $lines += "SET N" + $i + "=" + $ch.name
-    $lines += "SET U" + $i + "=" + $ch.url
-    $lines += "SET X" + $i + "=" + $(if ($ch.active) {"1"} else {"0"})
-    $i++
+    [IO.File]::WriteAllLines($TMP_V, $lines, [Text.Encoding]::UTF8)
+    exit 0
+} catch {
+    exit 1
 }
 
-[System.IO.File]::WriteAllLines($out, $lines, [System.Text.Encoding]::UTF8)
-Write-Host "OK"
